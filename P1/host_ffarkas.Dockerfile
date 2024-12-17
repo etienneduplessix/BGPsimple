9d29d1fdcb0790c5d0013ef367e021297476806c
farkasf <120285330+farkasf@users.noreply.github.com>
2024-12-17 16:31:40 +0100

    add dynamic multicast setup | edit Dockerfile

--- P1/host_ffarkas.Dockerfile
+++ P1/host_ffarkas.Dockerfile
@@ -2,4 +2,4 @@ FROM alpine:latest
 
 RUN apk update && apk upgrade
 
-RUN apk add --no-cache busybox
+RUN apk add --no-cache busybox iproute2
---
+++ P2/p2_dynamic.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+BLUE='\033[1;34m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+routers=("router_ffarkas-1" "router_ffarkas-2")
+containers=("host_ffarkas-1" "host_ffarkas-2" "router_ffarkas-1" "router_ffarkas-2")
+
+replace_static_setup() {
+    local IP_range=$1
+
+    for router_container in "${routers[@]}"; do
+        echo -e "\n${YELLOW}[ROUTER_RECONFIG]${NC} reconfiguring $router_container..."
+        set -x
+        docker exec "$router_container" sh -c "ip link delete vxlan10"
+        docker exec "$router_container" sh -c "ip link delete br0"
+
+        docker exec "$router_container" sh -c "ip link add vxlan10 type vxlan id 10 dstport 4789 dev eth0 group $IP_range"
+        docker exec "$router_container" sh -c "ip link set dev vxlan10 up"
+
+        docker exec "$router_container" sh -c "ip link add br0 type bridge"
+        docker exec "$router_container" sh -c "ip link set dev br0 up"
+
+        docker exec "$router_container" sh -c "ip link set eth1 master br0"
+        docker exec "$router_container" sh -c "ip link set vxlan10 master br0"
+        set +x
+    done
+}
+
+display_info() {
+    echo -e "\n${BLUE}[host_ffarkas-1]${NC}"
+    docker exec "${containers[0]}" sh -c "ifconfig eth1"
+    docker exec "${containers[0]}" sh -c "bridge fdb show dev br0"
+
+    echo -e "\n${BLUE}[host_ffarkas-2]${NC}"
+    docker exec "${containers[1]}" sh -c "ifconfig eth1"
+    docker exec "${containers[1]}" sh -c "bridge fdb show dev br0"
+
+    echo -e "\n${BLUE}[router_ffarkas-1]${NC}"
+    docker exec "${containers[2]}" sh -c "ip -d link show vxlan10"
+
+    echo -e "\n${BLUE}[router_ffarkas-2]${NC}"
+    docker exec "${containers[3]}" sh -c "ip -d link show vxlan10"
+
+    echo ""
+}
+
+replace_static_setup "239.1.1.1"
+
+echo -e "\n${GREEN}[ROUTER_RECONFIG]${NC} dynamic reconfiguration done"
+display_info
